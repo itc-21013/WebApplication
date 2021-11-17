fa21c1340cecd73bfba0c8e8c6ff557f33c3749e
itc-21013 <s21013@std.it-college.ac.jp>
2021-11-17 07:22:32 +0000

    master

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
-import React, { useState } from 'react
-import './App.css'
-
 const App = () => {
-  const [checkedValues, setCheckedValues] = useState([])
-  const handleChange = (e) => {
-    if (checkedValues.includes(e.target.value)) {
-      setCheckedValues(
-        checkedValues.filter((checkedValue) =>
-          checkedValue !== e.target.value)
-      )
-    } else {
-      setCheckedValues([...checkedValues, e.target.value])
-    }
-  }
   return (
-    <div className='App'>
-      <p>
-        現在選択されている値:<b>{checkedValues.join('、')}</b>
-      </p>
-      <label>
-        <input
-          type='checkebox'
-          value='マウス'
-          onChange={handleChange}
-          checked={checkedValues.includes('マウス')}
-        />
-        マウス
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          value='モニター'
-          onChange={handleChange}
-          checked={checkedValues.includes('モニター')}
-        />
-        モニター
-      </label>
-      <label>
-        <input
-          type='checkbox'
-          value='キーボード'
-          onChange={handleChange}
-          checked={checkedValues.includes('キーボード')}
-        />
-        キーボード
-      </label>
-    </div>
+    <>
+      <h1>Hello</h1>
+    </>
   )
 }
 export default App
