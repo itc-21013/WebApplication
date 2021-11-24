1f159e45f790e301546cd04870fee3c874c70927
itc-21013 <s21013@std.it-college.ac.jp>
2021-11-24 06:09:22 +0000

    rensyu

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 // 関数コンポーネント内で state を扱えるようにするため、React を import 時に useState を読み込む
-import React, { useState } from "react";
-import "./styles.css";
+import React, { useState } from 'react'
+import './styles.css'
 
 const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState("HTML");
+  const [selectedValue, setSelectedValue] = useState('HTML')
 
-  const handleChange = (e) => {
-    setSelectedValue(e.target.value);
-  };
+  const handleChange = e => {
+    setSelectedValue(e.target.value)
+  }
 
   return (
-    <div className="App">
+    <div className='App'>
       <p>
         現在選択されている値：
         <b>{selectedValue}</b>
       </p>
 
       <select value={selectedValue} onChange={handleChange}>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-        <option value="JavaScript">JavaScript</option>
+        <option value='HTML'>HTML</option>
+        <option value='CSS'>CSS</option>
+        <option value='JavaScript'>JavaScript</option>
       </select>
     </div>
-  );
-};
-
-export default function App() {
-  return <InputSelectBox />;
+  )
 }
 
+export default function App () {
+  return <InputSelectBox />
+}
