e33eb1a990733d552257f9a2689fc7711a29e5c5
itc-21013 <s21013@std.it-college.ac.jp>
2021-12-03 08:24:02 +0000

    pokemon

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
-import React, {Component} from 'react';
-import { CardList } from './card-list.component';
-import { SearchBox } from './search-box.component';
-import { NavigationBtn } from './back-to-top';
-import './App.css';
+import React, { Component } from 'react'
+import { CardList } from './card-list.component'
+import { SearchBox } from './search-box.component'
+import { NavigationBtn } from './back-to-top'
+import './App.css'
 
-class App extends Component{
-  constructor(){
-    super();
+class App extends Component {
+  constructor () {
+    super()
 
     this.state = {
       pokemons: [],
-      search:''
-    };
+      search: ''
+    }
   }
 
-  componentDidMount(){
+  componentDidMount () {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
-    .then(response => response.json())
-    .then(name => this.setState({pokemons:name.results}));
+      .then(response => response.json())
+      .then(name => this.setState({ pokemons: name.results }))
   }
 
-  handleChange=(e) => {
-    this.setState({search: e.target.value});
-  };
+  handleChange = e => {
+    this.setState({ search: e.target.value })
+  }
 
-  render(){
+  render () {
+    const { pokemons, search } = this.state
+    const fileteredPokemons = pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(search.toLowerCase())
+    )
 
-    const {pokemons, search } = this.state;
-    const fileteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
-
-    return(
-      <div className="App">
-        <h4><a href="https://www.linkedin.com/in/surojit-manna" target="_blank" noreferrer>Author</a></h4>
+    return (
+      <div className='App'>
+        <h4>
+          <a
+            href='https://www.linkedin.com/in/surojit-manna'
+            target='_blank'
+            noreferrer
+          >
+            Author
+          </a>
+        </h4>
         <h1>Pokemon Database</h1>
         <SearchBox
-          placeholder='Search Pokemon' 
-          handleChange= {this.handleChange}
+          placeholder='Search Pokemon'
+          handleChange={this.handleChange}
         />
-        <NavigationBtn/>
+        <NavigationBtn />
         <CardList pokemons={fileteredPokemons}></CardList>
-
       </div>
-    );
+    )
   }
 }
 
-export default App;
-DEV Community – A constructive and inclusive social network for software developers. With you every step of your journey.
-
-Built on Forem — the open source software that powers DEV and other inclusive communities.
-
-Made with love and Ruby on Rails. DEV Community © 2016 - 2021.
+export default App
